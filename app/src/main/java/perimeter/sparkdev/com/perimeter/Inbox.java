29d789687e855a13d8df837b12a4c1d7f053e247
vrive <vanessariveros25@gmail.com>
2018-09-18 23:15:56 -0400

    Created a listview row xml file

--- app/src/main/java/perimeter/sparkdev/com/perimeter/Inbox.java
+++ app/src/main/java/perimeter/sparkdev/com/perimeter/Inbox.java
@@ -2,12 +2,17 @@ package perimeter.sparkdev.com.perimeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
-public class Inbox extends AppCompatActivity {
+public class Inbox extends  AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
     }
+
+
 }
