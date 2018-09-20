0d0141b7057a5d52f6ee6802c6bba57fa558e44b
astridRiv <arive284@fiu.edu>
2018-09-20 16:56:44 -0400

    Making edit to creating methods to sign existing and new users

--- app/src/main/java/perimeter/sparkdev/com/perimeter/MainActivity.java
+++ app/src/main/java/perimeter/sparkdev/com/perimeter/MainActivity.java
@@ -1,13 +1,83 @@
 package perimeter.sparkdev.com.perimeter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    //Declaring an Instance
+    private FirebaseAuth mAuth;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       //Initializing the instance
+        mAuth = FirebaseAuth.getInstance();
+
+
+        String userName = "apple5@apple.com";
+        String passWord = "Password";
+
+        //Start [CREATE EMAIL]
+        mAuth.signInWithEmailAndPassword(userName,passWord)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(null,"createUserWithEmail: success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Log.d(null, "UserID " + user.getUid());
+                } else{
+                    Log.w(null, "createUserWithEmail: failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Auth failed oops", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+        //END [CREATE EMAIL]
+
+        //Start [SIGN EXISTING USERS]
+        mAuth.createUserWithEmailAndPassword(userName,passWord)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                public void onComplete(@NonNull Task<AuthResult> task){
+                    if (task.isSuccessful()){
+                        Log.d(null, "signin was success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                    } else {
+                        Log.w(null, "failure", task.getException());
+                        Toast.makeText(MainActivity.this, "AUth faile",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                }
+                );
+
     }
+
+
+
+
 }
