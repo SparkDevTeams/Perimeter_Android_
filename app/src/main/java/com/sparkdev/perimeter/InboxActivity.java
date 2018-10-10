1c6b8f7625eb34568445f23279df38dc21f4d4d2
Cassandra Zuria <czuria1@gmail.com>
2018-10-10 16:28:52 -0400

    Merged Vanessa's part to the Master Branch

---
+++ app/src/main/java/com/sparkdev/perimeter/InboxActivity.java
@@ -0,0 +1,54 @@
+package com.sparkdev.perimeter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InboxActivity extends  AppCompatActivity {
+    // Chat Room Images
+    private Integer[] images = {R.drawable.gc, R.drawable.ecs, R.drawable.pg6, R.drawable.pg5,
+        R.drawable.library, R.drawable.sasc, R.drawable.oe};
+    private ArrayList<Integer> images2 = new ArrayList<Integer>(Arrays.asList(images));
+
+    // Chat Room Names
+    private String[] names = {"GC", "ECS", "PG6", "PG5",
+                        "Library","SASC","OE"};
+    private ArrayList<String> names2 = new ArrayList<String>(Arrays.asList(names));
+
+    // Chat Room Last Message Sent
+    private String [] lastMessage = {"Whats for lunch?", "Im going to print now.", "Good luck on the test!"
+                           , "Anyone found an ID?", "Im going to print now.", "Good luck on the test!","Im in the lab."} ;
+    private ArrayList<String> lastMessage2 = new ArrayList<String>(Arrays.asList(lastMessage));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox);
+
+        // Get access to the activity's RecyclerView
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.messagesRecyclerView);
+
+        // Define the RecyclerView's default layout manager and orientation
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        // Create the InboxAdapter and supply the adapter with the data
+        InboxAdapter customAdapter = new InboxAdapter(this, names2, lastMessage2,images2);
+        recyclerView.setAdapter(customAdapter);
+
+        // Add the line divider between each row
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(recyclerView.getContext()
+                                                                            ,llm.getOrientation());
+        recyclerView.addItemDecoration(itemDecoration);
+
+    }
+
+}
+
+
---
+++ app/src/main/java/com/sparkdev/perimeter/InboxAdapter.java
@@ -0,0 +1,86 @@
+package com.sparkdev.perimeter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class InboxAdapter extends RecyclerView.Adapter<InboxAdapter.InboxViewHolder> {
+
+  private ArrayList<String> mTitle;  //titleTextView
+  private ArrayList<String> mLastMsg; //last message sent
+  private ArrayList<Integer> mImgs; //image
+  private Context mContext;
+
+  // InboxAdapter Constructor
+  public InboxAdapter(Context context, ArrayList list, ArrayList list2, ArrayList list3) {
+    this.mTitle = list;
+    this.mLastMsg = list2;
+    this.mImgs = list3;
+    mContext = context;
+
+  }
+
+  //inner class too retrieve the views
+  public class InboxViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public ImageView imageView;
+    public TextView titleTextView;
+    public TextView lastMsgTextView;
+    public RelativeLayout rowLayout;
+
+    public InboxViewHolder(View view) {
+      super(view);
+
+
+      imageView = (ImageView) view.findViewById(R.id.chatImage);
+      titleTextView = (TextView) view.findViewById(R.id.chatName);
+      lastMsgTextView = (TextView) view.findViewById(R.id.description);
+      rowLayout = (RelativeLayout) view.findViewById(R.id.rowItem);
+
+      itemView.setOnClickListener(this);
+
+    }
+
+    //on click listener for each row
+    @Override
+    public void onClick(View view) {
+      Intent intent = new Intent(mContext, TempActivity.class);
+      mContext.startActivity(intent);
+    }
+  }
+
+  @NonNull
+  @Override
+  public InboxViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    View inboxView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listview_row,
+        viewGroup, false);
+
+    //creates InboxViewHolder with the custom layout for the row
+    return new InboxViewHolder(inboxView);
+  }
+
+  @Override
+  public void onBindViewHolder(InboxAdapter.InboxViewHolder inboxViewHolder, int i) {
+    String a = mTitle.get(i);
+    String b = mLastMsg.get(i);
+    Integer c = mImgs.get(i);
+
+    inboxViewHolder.titleTextView.setText(a);
+    inboxViewHolder.lastMsgTextView.setText(b);
+    inboxViewHolder.imageView.setImageResource(c);
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return mTitle.size();
+  }
+}
