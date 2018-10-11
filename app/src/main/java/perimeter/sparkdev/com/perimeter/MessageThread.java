5ae9ac22b0bc87b9c2e216c09bea7d2b8451c809
rmoli039 <rmoli039@fiu.edu>
2018-10-11 17:55:29 -0400

    Basic RecyclerView

--- app/src/main/java/perimeter/sparkdev/com/perimeter/MessageThread.java
+++ app/src/main/java/perimeter/sparkdev/com/perimeter/MessageThread.java
@@ -13,8 +13,8 @@ public class MessageThread extends Activity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    private ArrayList <String> contacts = new ArrayList<>();
-    private ArrayList <String> messages = new ArrayList<>();
+    private ArrayList <String> mContacts = new ArrayList<>();
+    private ArrayList <String> mMessages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +22,14 @@ public class MessageThread extends Activity {
         setContentView(R.layout.activity_message_thread);
 
         for(int i = 0; i < 30; i++){
-            contacts.add("Contact " + (i+1));
-            messages.add("This is an example of a text message from Contact " + (i+1));
+            mContacts.add("Contact " + (i+1));
+            mMessages.add("This is an example of a text message from Contact " + (i+1));
         }
 
         // Get access to the RecyclerView
         mRecyclerView = (RecyclerView) findViewById(R.id.message_recycler_view);
         // Create the adapter and supply the adapter with the data (i.e from an arraylist or database)
-        mAdapter = new RecyclerAdapter(this,contacts);
+        mAdapter = new RecyclerAdapter(this,mContacts, mMessages);
         // Connect the adapter to the RecyclerView
         mRecyclerView.setAdapter(mAdapter);
         // Define the RecyclerView's default layout manager
