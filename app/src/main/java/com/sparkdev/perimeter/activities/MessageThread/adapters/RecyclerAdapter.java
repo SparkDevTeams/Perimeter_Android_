f1cf32a174c842db98f735df0efc2abbfb432806
rmoli039 <rmoli039@fiu.edu>
2018-10-16 16:13:27 -0400

    Updated Message RecyclerView

--- app/src/main/java/com/sparkdev/perimeter/activities/MessageThread/adapters/RecyclerAdapter.java
+++ app/src/main/java/com/sparkdev/perimeter/activities/MessageThread/adapters/RecyclerAdapter.java
@@ -37,7 +37,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         public MyViewHolder(View itemView, RecyclerAdapter adapter) {
             super(itemView);
             // Initialize the view holder's text view from the XML resources (activity_contact_list.xml)
-            // Be sure to cast it to the View type that you neesd it to be (i.e TextView)
+            // Be sure to cast it to the View type that you need it to be (i.e TextView)
             mContactTextView = (TextView) itemView.findViewById(R.id.contact_name);
             mMessageTextView = (TextView) itemView.findViewById(R.id.message_text);
             // Set up the adapter
@@ -59,10 +59,10 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     // The onBindViewHolder() connects your data to your view holder
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        String currentContact = mContacts.get(position);     // Hold the current contact name
-        String currentMessage = mMessages.get(position);     // Hold the current contact name
-        holder.mContactTextView.setText(currentContact); // Set contact name at i position to TextView
-        holder.mMessageTextView.setText(currentMessage); // Set contact name at i position to TextView
+        String currentContact = mContacts.get(position);    // Hold the current contact name
+        String currentMessage = mMessages.get(position);    // Hold the current contact name
+        holder.mContactTextView.setText(currentContact);    // Set contact name at i position to TextView
+        holder.mMessageTextView.setText(currentMessage);    // Set contact name at i position to TextView
     }
 
     // Return the size of your dataset (invoked by the layout manager)
