f50ee69d665fec76154a0eddcb76852159472112
Cassandra Zuria <czuria1@gmail.com>
2018-10-25 15:59:16 -0400

    Add Firebase listeners

---
+++ app/src/main/java/com/sparkdev/perimeter/activities/Firebase/LoginInterfaces/PerimeterLoginCompletionListener.java
@@ -0,0 +1,11 @@
+package com.sparkdev.perimeter.activities.Firebase.LoginInterfaces;
+
+public interface PerimeterLoginCompletionListener {
+
+
+    //method call when login is successful
+    public void onSuccess();
+
+    //method call when login is failure
+    public void onFailure();
+}
